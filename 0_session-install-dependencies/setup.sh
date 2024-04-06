c9fa9e1611d9aac47a7d22a45faf8d0c3adaba85
Albin George <ageorge@cloudera.com>
2024-04-06 19:17:44 -0400

    PDF

--- 0_session-install-dependencies/setup.sh
+++ 0_session-install-dependencies/setup.sh
@@ -1,7 +1,7 @@
 # set CMAKE_ARGS=-DLLAMA_CUBLAS=on
 # set FORCE_CMAKE=1
-# pip install llama-cpp-python==0.2.27 --force-reinstall --upgrade --no-cache-dir
+# pip install transformers==4.37.2 --force-reinstall --upgrade --no-cache-dir
 # CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install --upgrade --force-reinstall llama-cpp-python
 pip install llama-cpp-python --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
-pip install llama_index llama_index.core llama_index.vector_stores.pinecone llama_index.core  llama_index.llms.openai llama_index.embeddings.openai llama_index.embeddings.huggingface llama_index.llms.huggingface transformers llama_index.vector_stores.milvus huggingface_hub llama_index.llms.llama_cpp gradio torch milvus unstructured spacy llama_index.readers.nougat_ocr "unstructured[pdf]"
+pip install llama_index llama_index.core llama_index.vector_stores.pinecone llama_index.core  llama_index.llms.openai llama_index.embeddings.openai llama_index.embeddings.huggingface llama_index.llms.huggingface transformers==4.37.2 llama_index.vector_stores.milvus huggingface_hub llama_index.llms.llama_cpp gradio torch milvus unstructured spacy llama_index.readers.nougat_ocr "unstructured[pdf]"
 python -c "import nltk; nltk.download('averaged_perceptron_tagger')"
