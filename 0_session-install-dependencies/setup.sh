72aac6886bd0af1ebe6efc4eb25f0595816fe41b
Albin George <ageorge@cloudera.com>
2024-04-12 15:36:54 -0400

    fix script

--- 0_session-install-dependencies/setup.sh
+++ 0_session-install-dependencies/setup.sh
@@ -4,8 +4,10 @@ pip3 install https://${CDSW_DOMAIN}/api/v2/python.tar.gz
 
 if [ "${USE_ONLY_CPU}" == "true" ]
 then
+    echo "installing CPU only version of llama-cpp"
     pip install llama-cpp-python
 else
+    echo "installing GPU version of llama-cpp"
     pip install llama-cpp-python --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu122
 fi
 pip install llama_index llama_index.core llama_index.vector_stores.pinecone llama_index.core  llama_index.llms.openai llama_index.embeddings.openai llama_index.embeddings.huggingface llama_index.llms.huggingface transformers==4.37.2 llama_index.vector_stores.milvus huggingface_hub llama_index.llms.llama_cpp gradio torch milvus unstructured spacy llama_index.readers.nougat_ocr "unstructured[pdf]"
